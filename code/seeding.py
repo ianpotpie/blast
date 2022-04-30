2b5402e8da7a3c5cd903aa473cae574e8a088ea5
Ian Gordon <ianbgordon@gmail.com>
2022-04-30 02:05:50 -0400

    change hit mechanics and implemented hit merging

--- code/seeding.py
+++ code/seeding.py
@@ -1,86 +1,130 @@
 import sys
 import itertools
+import heapq
 from scoring_scheme import ScoringScheme
 from knuth_morris_pratt import kmp_search
 
 
-def get_neighborhoods(sequence, scoring_scheme, k, T):
+def get_seeds(seq: str, k: int):
     """
-    Gets all the k-mers that have a high-scoring subsequence within the sequence.
+    Retrieves all k-mers from a query sequence and returns them as a list of (index, kmer) tuples.
 
-    :param sequence: the query string
-    :param scoring_scheme: the system for scoring alignments
-    :param k: the size of the k-mers
-    :param T: the threshold score for seeds
-    :return: a dictionary of high-scoring kmers to their query indices
+    :param seq: the sequence from which seeds are extracted
+    :param k: the size of the seeds/kmers
+    :return: a list of seeds, (kmer,index) tuples
     """
-
-    symbols = scoring_scheme.get_symbols()
-    indices_by_kmer = {}
-    for kmer in itertools.product(symbols, repeat=k):
-        kmer = "".join(kmer)
-        indices_by_kmer[kmer] = []
-        for i in range(len(sequence) - k):
-            subsequence = sequence[i: i + k]
-            align_score = scoring_scheme.score(kmer, subsequence)
-            if align_score > T:
-                indices_by_kmer[kmer].append(i)
-        if len(indices_by_kmer[kmer]) == 0:
-            del indices_by_kmer[kmer]
-    return indices_by_kmer
-
-
-def seeding(database, query, scoring_scheme, k, T):
-    """
-    Given a database and a set of seeds, finds all exact matches of those seeds in the database.
-
-    :param database: a database of strings
-    :param query: a query sequence
-    :param scoring_scheme: an alignment scorer
-    :param k: the length of the kmers of the query
-    :param T: the minimum score of an alignment in the query to be considered a hit
-    :return: a dictionary of queries to their matches (a tuple of the sequence index and position in that sequence)
-    """
-
-    indices_by_kmer = get_neighborhoods(query, scoring_scheme, k, T)
-
     seeds = []
-    for nth_seq, sequence in enumerate(database):
-        for kmer, query_indices in indices_by_kmer.items():
-            hits = kmp_search(sequence, kmer)
-            for seq_index in hits:
-                for query_index in query_indices:
-                    seeds.append((nth_seq, seq_index, query_index))
-    seeds.sort()
-
+    for i in range((len(seq) + 1) - k):
+        kmer = seq[i:i + k]
+        seeds.append((i, kmer))
     return seeds
 
 
+def get_neighborhoods(seq: str, k: int, scoring_scheme: ScoringScheme, T: float):
+    """
+    Gets all the k-mers that have a high-scoring subsequence within the sequence and returns them as a list of
+    (index, kmer) tuples.
+
+    :param seq: the sequence from which seeds are extracted
+    :param k: the size of the k-mers/seeds
+    :param scoring_scheme: the system for scoring alignments
+    :param T: the threshold score for seeds
+    :return: a list of seeds, (kmer, index) tuples
+    """
+    seeds = []
+    symbols = scoring_scheme.get_symbols()
+    for i in range((len(seq) + 1) - k):
+        subseq = seq[i: i + k]
+        for kmer in itertools.product(symbols, repeat=k):
+            kmer = "".join(kmer)
+            if scoring_scheme(kmer, subseq) > T:
+                seeds.append((i, kmer))
+    return seeds
+
+
+def find_hits(db_seq: str, seeds: list):
+    """
+    Finds instances of the seeds in the database sequence.
+
+    :param db_seq: the database sequence
+    :param seeds: a list of seeds (query_index, kmer)
+    :return: a list of hits (db_index, query_index, length)
+    """
+    kmer_to_indices = {kmer: [] for i, kmer in seeds}  # dictionary prevents searching for the same kmer twice
+    for query_index, kmer in seeds:
+        kmer_to_indices[kmer].append(query_index)
+
+    hits = []
+    for seed, query_indices in kmer_to_indices.items():
+        k = len(seed)
+        db_indices = kmp_search(db_seq, seed)
+        for db_index in db_indices:
+            for query_index in query_indices:
+                hits.append((db_index, query_index, k))
+
+    return hits
+
+
+def combine_hits(hits, A):
+    """
+    Combines all hits with ends that either overlap or have ends within a distance of A on the diagonal.
+
+    :param hits: a list of seed-hit tuples (db_index, query_index, length)
+    :param A: a distance threshold for merging hits
+    :return: a new dictionary of hits to their
+    """
+    hits_by_diagonal = {}
+    for db_index, query_index, k in hits:
+        diagonal = db_index - query_index  # this defines the diagonal
+        if diagonal not in hits_by_diagonal:
+            hits_by_diagonal[diagonal] = []
+        heapq.heappush(hits_by_diagonal[diagonal], (db_index, query_index, k))  # hits will be sequential
+
+    combined_hits = []
+    for diagonal, aligned_hits in hits_by_diagonal.items():
+        start_i, start_j, combined_k = aligned_hits[0]
+        prev_i, prev_j = start_i, start_j
+        for i, j, k in aligned_hits:
+            if i - prev_i <= A:
+                combined_k = (i - start_i) + k
+            else:
+                combined_hits.append((start_i, start_j, combined_k))
+                start_i, start_j, combined_k = i, j, k
+            prev_i, prev_j = i, j
+
+    return combined_hits
+
+
+def extend_hits(hits, X, S):
+    pass
+
+
 def main():
-    database_file = sys.argv[1]
-    with open(database_file) as f:
-        database = [line.strip() for line in f.readlines()]
+    # the database file should contain sequences seperated by "\n"
+    db_seq = sys.argv[1]
 
-    query_file = sys.argv[2]
-    with open(query_file) as f:
-        query = f.readline().strip()
+    # the query file should contain a single query string
+    query_seq = sys.argv[2]
 
+    # the matrix file contains the scoring matrix
     matrix_file = sys.argv[3]
     scoring_scheme = ScoringScheme()
     scoring_scheme.load_matrix(matrix_file)
 
+    # this is the size of the seeds/kmers
     k = int(sys.argv[4])
 
+    # the minimum alignment score (we are guaranteed alignments over this threshold)
     T = float(sys.argv[5])
 
-    seeds = seeding(database, query, scoring_scheme, k, t)
+    seeds = seeding(database, query, scoring_scheme, k, T)
 
     print(query)
     print(k)
     print(int(T))
     print(len(seeds))
-    for nth_seq, i_pos, query_index in seeds:
-        print(f"Sequence {i_seq} Position {i_pos} Q-index {i_query}")
+    for nth_seq, seq_index, query_index in seeds:
+        print(f"Sequence {nth_seq} Position {seq_index} Q-index {query_index}")
 
 
 if __name__ == "__main__":
